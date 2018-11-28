0c1dd2b8c55516426e45aa569302281d4e9b868c
Javier <j.aldana.corchon@gmail.com>
2018-11-29 00:54:37 +0100

    variable de entorno en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 const urlDB = process.env.NODE_ENV === 'dev' ?
 'mongodb://localhost:27017/cafe':
-'mongodb://cafe-user:123456a@ds019654.mlab.com:19654/cafe'
+process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
